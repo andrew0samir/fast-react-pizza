8db1fecde067ec8b7ccd7ce2dcc5681f4e8b58c8
andrew0samir <andrewsamir317@gmail.com>
2025-07-21 17:56:30 +0300

    created app layout , handle fetching menu data in router

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/"> Fast React Pizza CO.</Link>
+
+      <p>Logo</p>
+    </header>
+  );
+}
+
+export default Header;
