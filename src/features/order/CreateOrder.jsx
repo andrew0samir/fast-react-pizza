b61c3504f84e27bb4b6a537efc96177a80abf25b
andrew0samir <andrewsamir317@gmail.com>
2025-07-29 23:04:55 +0300

    reusing styles with apply directive

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -48,13 +48,23 @@ function CreateOrder() {
             <Form method="POST">
                 <div>
                     <label>First Name</label>
-                    <input type="text" name="customer" required />
+                    <input
+                        type="text"
+                        name="customer"
+                        required
+                        className="input"
+                    />
                 </div>
 
                 <div>
                     <label>Phone number</label>
                     <div>
-                        <input type="tel" name="phone" required />
+                        <input
+                            type="tel"
+                            name="phone"
+                            required
+                            className="input"
+                        />
                     </div>
                     {formErrors?.phone && <p>{formErrors.phone}</p>}
                 </div>
@@ -66,7 +76,7 @@ function CreateOrder() {
                             type="text"
                             name="address"
                             required
-                            className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+                            className="input"
                         />
                     </div>
                 </div>
