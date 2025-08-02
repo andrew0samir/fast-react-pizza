2056b7a0713d9c8c57d7a8a3131635e4ca44dea9
andrew0samir <andrewsamir317@gmail.com>
2025-08-03 00:31:25 +0300

    configuring tailwind , making custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,17 @@ export default {
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
-      ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    ],
+    theme: {
+        fontFamily: {
+            sans: "Roboto Mono , monospace",
+        },
+        extend: {
+            height:{
+                screen : "100dvh"
+            }
+        },
+    },
+    plugins: [],
 }
 
