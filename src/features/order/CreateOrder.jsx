45bfe80a732527d1349231ec8947edfb50357c95
andrew0samir <andrewsamir317@gmail.com>
2025-08-04 20:44:06 +0300

    styling the menu , changed the button to have types

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -102,7 +102,7 @@ function CreateOrder() {
                         name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <Button disabled={isSubmitting}>
+                    <Button disabled={isSubmitting} type="primary">
                         {isSubmitting ? 'Placting order...' : 'Order now'}
                     </Button>
                 </div>
